de1e6dc69e87cffc87ab54713243bda464aebcf7
Cerne17 <miguelcerne14@gmail.com>
2025-01-10 14:16:04 -0300

    feat: error handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"golang-crud/helper"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -13,4 +14,8 @@ func main() {
 	routes := httprouter.New()
 
 	server := http.Server{Addr: "localhost:8888", Handler: routes}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+
 }
